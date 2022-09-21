3b0d458e8bfd95f1bb3933530968ed9dd8329dfc
Johnny Wright <johnnywrightiv@gmail.com>
2022-09-21 17:01:20 -0500

    completed first two problems

--- code_challenge/submission.js
+++ code_challenge/submission.js
@@ -1,11 +1,30 @@
+const testCase = ['a', 'b', 'c', 'a', 'b', 'c', 'a', 'a', 'd'];
+
 const findSum = function(array) {
   // your code here - don't forget to return a number!
-  
+  const sum = array.reduce((a, b) => a + b);
+  return sum;
 };
 
 const findFrequency = function(array) {
   // your code here - don't forget to return an object!
-};
+  // OUTPUT: {most: 'a', least: 'd'}
+  const frequencies = {
+    most: '',
+    least: ''
+  };
+  
+  const obj = {};
+  array.forEach(element => {
+    if(!obj[element]) obj[element] = 1;
+    else obj[element]++;
+  });
+
+  frequencies.most = Object.keys(obj)[0];
+  frequencies.least = Object.keys(obj)[Object.keys(obj).length - 1];
+
+  return frequencies;
+  };
 
 const isPalindrome = function(str) {
   // your code here - don't forget to return a boolean!
@@ -22,3 +41,7 @@ const removeParenth = function(str) {
 const scoreScrabble = function(str) {
   // your code here - don't forget to return a number!
 };
+
+console.log('hi')
+// console.log(findSum(testCase))
+console.log(findFrequency(testCase))
